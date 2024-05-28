5efdf78580a3d22a20eb51c2e98a85769202e251
Jerin Sam <Jerin.sam@alignedautomation.com>
2024-05-28 21:55:29 +0530

    updated config

--- install_and_config/3-confluent-hub-configure/2-add-to-env-variable.sh
+++ install_and_config/3-confluent-hub-configure/2-add-to-env-variable.sh
@@ -2,10 +2,17 @@
 # 1. /etc/profile – The profile file within the “/etc/” directory allows you to modify system-wide environment variables for logged-in users.
 # 2. ~/.profile – The “.profile” file allows you to change the PATH variable for a specific user.
 
-# execute below script to update the environmental variable path
+# Open the .profile file in nano editor
 nano ~/.profile
 
-# Add following confluent-hub path to environmental variable
-PATH="/home/jerin/learn/sql-server-to-redis-using-kafka-connect/install_and_config/3-confluent-hub-configure/confluent-hub/bin:$PATH"
+# Add the following line to update the PATH environment variable
+# (Place this line at the end of the file)
+export PATH="/home/jerin/learn/sql-server-to-redis-using-kafka-connect/install_and_config/3-confluent-hub-configure/confluent-hub/bin:$PATH"
 
-# Close and save the editor
+# Save and close the file (in nano, you can do this by pressing Ctrl+O to save and Ctrl+X to exit)
+
+# Source the .profile to apply the changes immediately
+source ~/.profile
+
+# Optional: Verify the update by checking the PATH variable
+echo $PATH
